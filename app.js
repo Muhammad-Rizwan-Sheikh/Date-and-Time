3de9a4ebda8968239ba61b1a6e69244f89545762
MUHAMMADRIZWAN <riwazkhan334@gmail.com>
2024-05-10 17:47:20 +0500

    Date and Time

---
+++ app.js
@@ -0,0 +1,109 @@
+// let now = new Date();
+
+// console.log(now);   
+
+
+// let dayName = ["Sun" , "Mon" , "Tue" , "Wed", "Thur" , "Fri" , 'Sat']
+
+// let day = now.getDay();
+// console.log(day);
+// console.log(dayName[day])
+
+
+
+// let MonthName = ["Jan" , "Feb" , "Mar" , "Apr", "May" , "Jun" , 'Jul', "Aug" , "Sep" , "Oct" , "Nov" , "Dec"]
+
+// let month = now.getMonth();
+// console.log(day);
+// console.log(MonthName[month])
+
+// let now = new Date();
+// console.log(now);
+// let timeDuration = [ "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" ,"Satarday" , 
+// "Sunday"]
+
+// let month = now.getMonth() + now.getFullYear;
+// let time = now.getTime();
+// console.log(timeDuration)
+
+
+
+
+
+
+
+
+// let ms = now.getMilliseconds();
+
+// console.log(ms);
+
+
+
+// let msNow = now.getMiliseconds();
+// console.log(msNow[])
+
+
+// let dob = new Date ("August 18 2004")
+// let now = new Date();
+
+// let age= now.getTime() - dob.getTime()  ;
+// console.log( Math.floor(age / 1000 / 60 / 60 /24 / 365.25));
+
+
+
+
+// let today = new Date ();
+// let startDate= new Date ("December 11, 2023");
+
+// let diff = today - startDate;
+
+// console.log(diff /1000 / 60/ 60 /24 /30); // milisecond / seconds / minutes / hourse / days;
+
+
+
+// let dob = new Date ("August 18, 2004");
+
+// console.log(dob);
+
+// dob.setDate(14);
+
+// dob.setFullYear(2007);
+// console.log(dob);
+
+
+
+
+
+
+
+
+
+// function calculateHyp (base , prep){
+//       function calculateSqr(num){
+//             return num * num ;
+//       } 
+
+
+//     let hyp2 = calculateSqr(base) + calculateSqr(prep);
+//     let hyp = Math.sqrt(hyp2)
+    
+//     console.log(4,5)
+// }
+
+
+
+
+
+
+function findLargeNSmallNSum (){
+console.log(arguments);
+for(let i = 1 ; i < arguments.length ; i++){
+          let sum =  0
+          sum     
+}
+
+}
+
+findLargeNSmallNSum(1,2,3);
+findLargeNSmallNSum(1,2,3,7,9,20,45,66,100,999);
+findLargeNSmallNSum();
